0b17d9974777f98aa1e72513fb405ff042ef9c20
Santino Valenzuela <s0nnyv123@gmail.com>
2022-03-05 01:36:04 -0500

    Update - Using fetch api to fetch random data api with useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (
+      async () => {
+        let userData;
+
+        try {
+          const response = await fetch('https://random-data-api.com/api/users/random_user?size=3');
+          userData = await response.json();
+          console.log(userData);
+        }
+        catch (error) {
+          console.log(error);
+          userData = [];
+        }
+
+        setUsers(userData);
+      }
+    )();
+  }, []);
+
   return (
     <div className="App">
 
